a737ee1e5c86393dc091de4adba5f51e919b5a7d
Jonathan Gomez <jongomezdev@gmail.com>
2020-12-16 12:22:54 -0500

    Link js file, grab dom elements and create the breathe animation function

--- breathing.js
+++ breathing.js
@@ -1,1 +1,22 @@
-alert("hello world");
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+console.log(breatheTime, holdTime);
+
+breathAnimation();
+
+function breathAnimation() {
+  console.log("breathe in");
+
+  setTimeout(() => {
+    console.log("hold");
+
+    setTimeout(() => {
+      console.log("breathe out");
+    }, holdTime);
+  }, breatheTime);
+}
