3b295f591e7c348eb12f71c0021da7325e5e3410
Jonathan Gomez <jongomezdev@gmail.com>
2020-12-16 13:18:33 -0500

    Complete JS file, messing around with a few colors in css

--- breathing.js
+++ breathing.js
@@ -11,12 +11,14 @@ breathAnimation();
 
 function breathAnimation() {
   text.innerText = "Breathe In";
+  container.className = "container grow";
 
   setTimeout(() => {
     text.innerText = "Hold";
 
     setTimeout(() => {
       text.innerText = "Breathe Out";
+      container.className = "container shrink";
     }, holdTime);
   }, breatheTime);
 }
